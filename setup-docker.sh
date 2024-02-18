65a590a04a140a1ea7fa9aa6a2947e2264bc9b8a
morph <duncansantiago18@gmail.com>
2024-02-18 05:54:24 -0500

    Initial code setup

---
+++ setup-docker.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# set debug mode
+set -x
+
+setup_docker(){
+    :
+}
+
+read_package_list(){
+    :
+}
+
+install_packages(){
+    :
+}
+
+init(){
+    :
+}
+
+
+init "${@}"
